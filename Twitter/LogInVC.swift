0abae1cd0bfc9c2dc5fe0fc96f4ea8c054baddbe
Orlando Vargas <Orlandov87@live.com>
2021-09-24 19:52:03 -0400

    Converted from Storyboard to Scenes

---
+++ Twitter/LogInVC.swift
@@ -0,0 +1,30 @@
+//
+//  LogInVC.swift
+//  Twitter
+//
+//  Created by Orlando Vargas on 9/24/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
