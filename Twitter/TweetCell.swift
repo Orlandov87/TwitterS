e6277933b3b3442bce82ed43def4bd84a7371eb1
Orlando Vargas <Orlandov87@live.com>
2021-10-02 20:07:57 -0400

    Finished main part

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -17,12 +17,23 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var favButton: UIButton!
     
     @IBOutlet weak var retweetButton: UIButton!
+    
     var favorited: Bool = false
+    var retweeted: Bool = false
+    var tweetId: Int = -1
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        favButton.titleLabel?.text = ""
-        retweetButton.titleLabel?.text = ""
+    }
+    
+    func setFavorite(_ isFavorited: Bool) {
+        favorited = isFavorited
+        
+        if(favorited) {
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: .normal)
+        } else {
+            favButton.setImage(UIImage(named: "favor-icon"), for: .normal)
+        }
     }
 
 
@@ -32,9 +43,38 @@ class TweetCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func setRetweeted(_ isRetweeted: Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: .normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: .normal)
+            retweetButton.isEnabled = true
+        }
+    }
+    
     @IBAction func favoriteTweet(_ sender: Any) {
+        let fav = !favorited
+        if(fav) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { Error in
+                print("Favorite did not succeed: \(Error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { Error in
+                print("Unfavorite did not succeed: \(Error)")
+            })
+        }
     }
     
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { error in
+            print("Error in retweeting: \(error)")
+        })
     }
 }
